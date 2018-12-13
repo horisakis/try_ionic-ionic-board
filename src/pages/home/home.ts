31851cc39ce13b6122fdee90913f05573ebb565e
horisaki <seiji.horisaki@gmail.com>
2018-12-13 22:44:25 +0900

    5.2.1 複数件の場合

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,6 +17,19 @@ export class HomePage {
     createdDate: '10分前'
   };
 
+  posts: { userName: string, message: string, createdDate: any}[]
+  = [
+    {
+      userName: 'Taro Yamada',
+      message: 'これはテストメッセージです',
+      createdDate: '10分前'
+      },
+      {
+        userName: 'Jiro Suzuki',
+        message: '２つ目のテストメッセージ',
+        createdDate: '5分前'
+      }
+  ]
   constructor(public navCtrl: NavController) {
 
   }
