7b6ae160192fbb82d481b7374f215747ecb630cf
horisaki <seiji.horisaki@gmail.com>
2018-12-21 19:18:29 +0900

    6.4 Firebaseモジュール追加

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD7bqsqS6hhCBdrasNFyXI5usLh9VC48eA",
+        authDomain: "ionic-board-187b5.firebaseapp.com",
+        databaseURL: "https://ionic-board-187b5.firebaseio.com",
+        projectId: "ionic-board-187b5",
+        storageBucket: "ionic-board-187b5.appspot.com",
+        messagingSenderId: "69810166548"
+    },
+};
